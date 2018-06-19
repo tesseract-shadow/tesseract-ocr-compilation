cb75e20259d586b7c5a4b9e81d3f99ecec66724e
hoto17296 <mail@hoto.me>
2018-06-19 17:55:29 +0900

    fix: test image url returns "404 not found"

--- scripts/6-test-ocr.sh
+++ scripts/6-test-ocr.sh
@@ -10,11 +10,11 @@ docker exec -it t4cmp tesseract \--list-langs
 # Base OCR test
 docker exec -it t4cmp /bin/bash -c "echo ====; mkdir ./ocr-files/; cd ./ocr-files/; pwd; \
 rm phototest.*; echo ====; \
-echo downloading test file:; wget -O phototest.tif https://github.com/tesseract-ocr/tesseract/raw/master/testing/phototest.tif; \
+echo downloading test file:; wget -O phototest.tif https://github.com/tesseract-ocr/test/raw/master/testing/phototest.tif; \
 tesseract phototest.tif phototest; \
 head -100 phototest.txt; \
 echo Compare the text above ^^^ with the source image: \ 
-echo https://github.com/tesseract-ocr/tesseract/raw/master/testing/phototest.tif"
+echo https://github.com/tesseract-ocr/test/raw/master/testing/phototest.tif"
 
 # Process and copy files to ./ocr-files/ directory
 docker exec -it t4cmp /bin/bash -c "cd ./ocr-files/; tesseract phototest.tif phototest -l eng --psm 1 --oem 2 pdf hocr"
