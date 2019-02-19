f37c67963899cebc044d4904ef1e1a8665f2462f
Daniel Black <daniel@linux.ibm.com>
2019-02-20 07:46:21 +1100

    build manifests for every release

---
+++ .travis-manifest.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [[ "$TRAVIS_EVENT_TYPE" != "pull_request" ]] &&
+   [[ "$TRAVIS_BRANCH" = "master" ]]; then
+     mkdir -p ~/.docker
+     echo '{ "experimental" : "enabled" }' > ~/.docker/config.json
+     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+     docker manifest create --amend "${REPO}:${RELEASE}" "${REPO}:${RELEASE}_x86_64" "${REPO}:${RELEASE}_ppc64le"
+     docker manifest inspect "${REPO}:${RELEASE}"
+     docker manifest push    "${REPO}:${RELEASE}"
+fi
